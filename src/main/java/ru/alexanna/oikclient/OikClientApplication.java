667f498c4e74dad7404905aa332c9df55978f843
Oleg Shulyakov <43452558+AleksOl31@users.noreply.github.com>
2024-04-26 15:22:28 +0300

    Initial commit

---
+++ src/main/java/ru/alexanna/oikclient/OikClientApplication.java
@@ -0,0 +1,13 @@
+package ru.alexanna.oikclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OikClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OikClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/alexanna/oikclient/OikClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alexanna.oikclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OikClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
